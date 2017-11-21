36684d29119304d8d06b93b6241f053514408f79
Timur Gafiulin <nem00700@gmail.com>
2017-11-21 22:36:17 +0300

    add routing for exit game in GameField component

--- src/components/GameField.js
+++ src/components/GameField.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class GameField extends Component {
 
     render() {
-        return <div>Game Field</div>;
+        return <div>
+            <div>Game Field</div>
+            <button><Link to="/">Exit Game</Link></button>
+
+        </div>;
     }
 }
 
