f57a96702cdd51c3f9117c15c1c0a91c2c4df503
Timur Gafiulin <nem00700@gmail.com>
2017-11-21 22:58:37 +0300

    add style for inputs in join game and new game components

--- src/components/JoinGame.js
+++ src/components/JoinGame.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 class JoinGame extends Component {
     render() {
         return <div className="join-game-block">
-                <input placeholder="Username"/>
-                <input placeholder="Game token" />
+                <input className="join-game-block__input" placeholder="Username"/>
+                <input className="join-game-block__input" placeholder="Game token" />
                 <Link to="/startGame"><button className="nav">Start Game</button></Link>
             </div>;
     }
--- src/components/NewGame.js
+++ src/components/NewGame.js
@@ -6,8 +6,8 @@ class NewGame extends Component {
 
     render() {
         return <div className="new-game-block">
-                <input placeholder="Username"/>
-                <input placeholder="Size of Game Field" />
+                <input className="new-game-block__input" placeholder="Username"/>
+                <input className="new-game-block__input" placeholder="Size of Game Field" />
                 <Link to="/startGame"><button className="nav">Start Game</button></Link>
             </div>;
     }
