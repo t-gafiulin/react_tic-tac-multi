e73651bab5aa2e7655797f6b7f63280d52dd265c
Timur Gafiulin <nem00700@gmail.com>
2017-11-22 22:15:51 +0300

    add state for JoinGame and NewGame components

--- src/components/JoinGame.js
+++ src/components/JoinGame.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class JoinGame extends Component {
+    state = {
+        username: "",
+        game_token: ""
+    }
+
+    handleChange(param, event){
+        this.setState({[param]: event.target.value});
+    }
+
     render() {
         return <div className="join-game-block">
-                <input className="join-game-block__input" placeholder="Username"/>
-                <input className="join-game-block__input" placeholder="Game token" />
+                <input 
+                    className="join-game-block__input" 
+                    placeholder="Username"
+                    onChange={this.handleChange.bind(this, "username")}
+                />
+                <input 
+                    className="join-game-block__input" 
+                    placeholder="Game token" 
+                    onChange={this.handleChange.bind(this, "game_token")}
+                />
                 <Link to="/startGame"><button className="nav">Start Game</button></Link>
             </div>;
     }
--- src/components/NewGame.js
+++ src/components/NewGame.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class NewGame extends Component {
+    state = {
+        username: "",
+        size: ""
+    }
 
+    handleChange(param, event){
+        this.setState({[param]: event.target.value});
+    }
 
     render() {
         return <div className="new-game-block">
-                <input className="new-game-block__input" placeholder="Username"/>
-                <input className="new-game-block__input" placeholder="Size of Game Field" />
+                <input 
+                    className="new-game-block__input" 
+                    placeholder="Username"
+                    onChange={this.handleChange.bind(this, "username")}
+                />
+                <input 
+                    className="new-game-block__input" 
+                    placeholder="Size of Game Field"
+                    onChange={this.handleChange.bind(this, "size")}
+                />
                 <Link to="/startGame"><button className="nav">Start Game</button></Link>
             </div>;
     }
