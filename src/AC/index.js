e1d7d1b4141e75f29953a4e5602e02eb0dd030dc
Timur Gafiulin <nem00700@gmail.com>
2017-11-23 19:27:11 +0300

    add reducers for game

--- src/AC/index.js
+++ src/AC/index.js
@@ -3,15 +3,19 @@ import { CREATE_GAME, JOIN_GAME } from '../constants';
 export function createGame(username, size){
     return {
         type: CREATE_GAME,
-        size: size,
-        username: username,
+        payload: {
+            size: size,
+            username: username,
+        }
     }
 }
 
 export function joinGame(username, game_token){
     return {
         type: JOIN_GAME,
-        username: username,
-        token: game_token,
+        payload: {
+            username: username,
+            token: game_token,
+        }
     }
 }
---
+++ src/reducer/game.js
@@ -0,0 +1,40 @@
+import { CREATE_GAME, JOIN_GAME } from '../constants';
+
+const initialState = {
+    '123abc': {
+        create_username: '',
+        join_username: '',
+        size_gamefield: ''  
+    },
+}
+
+export default function game ( state = initialState, action ){
+    const { type, payload } = action;
+
+    switch ( type ){
+        case CREATE_GAME:
+            const { username, size } = payload;
+
+            return {
+                ...state,
+                ['123abc']: {
+                    ...state['123abc'],
+                    create_username: username, 
+                    size_gamefield: size
+                }
+            }
+
+        case JOIN_GAME:
+            const { username, token } = payload;
+        
+                    return {
+                        ...state,
+                        ['123abc']: {
+                            ...state['123abc'],
+                            join_username: username, 
+                        }
+                    }
+        default:
+            return state;
+    }
+}
