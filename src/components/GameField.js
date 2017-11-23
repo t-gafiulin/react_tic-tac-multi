0421820c30df593a7c2401125c2d364081608380
Timur Gafiulin <nem00700@gmail.com>
2017-11-23 22:25:38 +0300

    add connect for gamefield component

--- src/components/GameField.js
+++ src/components/GameField.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class GameField extends Component {
 
     render() {
         const params = this.props.match.params;
-        console.log(params);
+        const { create_username, join_username, size_gamefield } = this.props.game['123abc'];
+        console.log(this.props.game['123abc']);
 
         return <div className="gamefield-block">
-            <div>{params.param}</div>
+            <div>Creator: { create_username }</div>
+            <div>Creator: { join_username }</div>
+            <div>Creator: { size_gamefield }</div>
             <Link to="/"><button className="nav">Exit Game</button></Link>
 
         </div>;
     }
 }
 
-export default GameField;
+//export default GameField;
+
+export default connect (
+    state => ({
+        game: state.game,
+    })
+)(GameField);
+
